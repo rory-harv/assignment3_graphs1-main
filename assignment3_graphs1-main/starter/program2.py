86396f95c7040226ca218af4b9874f32d583f38b
rory <victoria.harvey1@icloud.com>
2025-10-28 11:08:51 -0700

    final updates to algorithms

--- assignment3_graphs1-main/starter/program2.py
+++ assignment3_graphs1-main/starter/program2.py
@@ -106,7 +106,7 @@ def reconstruct_path(parent: dict[IVertex], start: IVertex, goal: IVertex, verti
 
 
 
-    print(f'Path from {start.get_name()} to {goal.get_name()}: ')
+    print(f'Path from {start.get_name()} to {goal.get_name()}: ')   # finalized print out
     for v in path:
         print(f'-> {v.get_name()}')
     print()
@@ -120,6 +120,7 @@ def reconstruct_path(parent: dict[IVertex], start: IVertex, goal: IVertex, verti
 # dijkstra's algorithm
 
 def dijkstra(graph: IGraph, start: IVertex, goal: IVertex) -> None:
+    '''Dijkstra's Algorithm using g(n) values to search graph.'''
     frontier = PriorityQueue()
     frontier.put((0, start))  # adds start vertex to the frontier
     explored: List[IVertex] = []
@@ -207,28 +208,32 @@ def greedy_bfs(graph: IGraph, start: IVertex, goal: IVertex) -> None:
 # a* algorithm
 
 def a_star(graph: IGraph, start: IVertex, goal: IVertex):
+    '''A* Pathfinding Algorithm that searches using f(n) values.'''
     frontier = PriorityQueue()
     file_path = "starter\\vertices_v1.txt"  # file path containing data for coordinates
     h_val = get_heuristic(file_path, start, None)
-    frontier.put(start, h_val)  # adds start vertex to the frontier
+    frontier.put((h_val, start))  # adds start vertex to the frontier
     explored: List[IVertex] = []
-    g_scores: dict[IVertex, int] = {start: 0}
-    f_scores: dict[IVertex, int] = {}
+    cost_so_far: dict[IVertex, float] = {}
+    f_scores: dict[IVertex, float] = {}
     parent: dict[IVertex] = {}
     vertices_explored: int = 0
     edges_explored: int = 0
 
+    parent[start] = None
+    cost_so_far[start] = 0
+
     start_time = time.perf_counter()
 
     while frontier:
-        first = frontier.get()
+        first = frontier.get()  # pops lowest f(n)
         current = first[1]
         vertices_explored += 1
         if current == goal:
             end_time = time.perf_counter()
-            elapsed = end_time - start_time
-            reconstruct_path(parent, start, goal, vertices_explored, edges_explored, elapsed)
-            restart()
+            elapsed = end_time - start_time # marks overall execution time
+            reconstruct_path(parent, start, goal, vertices_explored, edges_explored, elapsed)   # reconstructs final path
+            restart()   # initiates restart
             return ""
         explored.append(current)
         edges = current.get_edges() # evaluates neighbors for the vertex
@@ -238,15 +243,14 @@ def a_star(graph: IGraph, start: IVertex, goal: IVertex):
                 neighbors.append(edge.get_destination())
                 edges_explored += 1
         for neighbor in neighbors:
-            index = neighbors.index(neighbor)
-            tentative_g = g_scores[current] + get_cost(graph, current, neighbors[index])    # calculates new g(n)
-            if neighbor not in explored:
-                if neighbor not in frontier.queue or tentative_g < g_scores[neighbor]:
-                    g_scores[neighbor] = tentative_g
+            tentative_g = cost_so_far[current] + get_cost(graph, current, neighbor)    # calculates new g(n)
+            if neighbor not in explored:    # checks if neighbor already evaluated
+                if neighbor not in frontier.queue or tentative_g < cost_so_far[neighbor]:
+                    cost_so_far[neighbor] = tentative_g
                     h_val = get_heuristic(file_path, current, neighbor)
-                    f_scores[neighbor] = g_scores[neighbor] + h_val
+                    f_scores[neighbor] = cost_so_far[neighbor] + h_val  # updates f(n)
                     parent[neighbor] = current
-                    frontier.put(neighbor, f_scores[neighbor])
+                    frontier.put((f_scores[neighbor], neighbor))
     print(f'Path from {start.get_name()} to {goal.get_name()} not found. ') # returns failure if no path found
 
 
@@ -263,6 +267,19 @@ def restart():
 
 def main() -> None:
     graph: IGraph = read_graph("starter\\graph_v2.txt")
+
+    print("Welcome to Oregon Pathfinder!")
+    print()
+    print("Select Pathfinding Algorithm:")
+    print("1. Dijkstra's Algorithm")
+    print("2. Greedy Best-First Search")
+    print("3. A* Algorithm")
+    choice: int = int(input("Enter Choice (1-3): "))
+
+    if choice != 1 and choice !=2 and choice != 3:
+        choice: int = int(input("Please Enter Valid Choice (1-3): "))
+
+
     start_vertex_name: str  = input("Enter the start vertex name: ")
     goal_vertex_name: str = input("Enter the goal vertex name: ")
 
@@ -280,7 +297,13 @@ def main() -> None:
         print("Goal vertex not found")
         return
     
-    print(dijkstra(graph, start_vertex, goal_vertex))
+    if choice == 1:
+        print(dijkstra(graph, start_vertex, goal_vertex))
+    elif choice == 2:
+        print(greedy_bfs(graph, start_vertex, goal_vertex))
+    elif choice == 3:
+        print(a_star(graph, start_vertex, goal_vertex))
+    
 
 if __name__ == '__main__':
     main()
